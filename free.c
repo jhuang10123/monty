817162a64ec7fe30779139460c01a720f111a59a
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-17 03:59:21 -0700

    *stack

--- free.c
+++ free.c
@@ -7,7 +7,7 @@ void free_stack(stack_t **stack)
 {
 	stack_t *temp;
 
-        while (stack != NULL)
+        while (*stack != NULL)
         {
                 temp = *stack;
                 *stack = (*stack)->prev;
